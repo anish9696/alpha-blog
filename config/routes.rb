5f42c7033eff5c0d40aef6b66c339f6ebee2987b
anish9696 <anish.9696@gmail.com>
2021-11-23 18:57:12 +0530

    showing articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-   get 'about', to: 'pages#about'
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :articles, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
